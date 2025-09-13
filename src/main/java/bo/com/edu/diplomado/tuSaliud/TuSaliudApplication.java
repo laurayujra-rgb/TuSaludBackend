6f348770dceb4e1b5673bc65cf9372a139b015ad
laurayujra-rgb <laura.yujra@ucb.edu.bo>
2025-09-12 22:30:52 -0400

    first commit
    first commit

---
+++ src/main/java/bo/com/edu/diplomado/tuSaliud/TuSaliudApplication.java
@@ -0,0 +1,13 @@
+package bo.com.edu.diplomado.tuSaliud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuSaliudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuSaliudApplication.class, args);
+	}
+
+}
